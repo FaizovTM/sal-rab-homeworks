ab70435d10e3ef3737f14a39b4d3c1e4dbb17fd7
Timur Faizov <zorki2020@gmail.com>
2022-02-01 21:42:16 +0500

    task 2 Done

--- task_2/solution.js
+++ task_2/solution.js
@@ -15,7 +15,10 @@ function calcShipping(sum, min, shipping) {
 
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
-
+let shippingSum;
+if (productsSum == 0 || productsSum >= freeShippingMinSum) shippingSum = 0;
+else if (productsSum > 0 && productsSum < freeShippingMinSum) shippingSum = shippingPrice;
+console.log(shippingSum);
     // Конец решения задания №2.1.
 
     return shippingSum;
@@ -33,7 +36,10 @@ function calcDiscount(sum, min, discount) {
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
-
+let discountSum;
+if (productsSum >= discountMinSum) discountSum = discountPart / 100 * productsSum; 
+else discountSum = 0;
+console.log(discountSum);
     // Конец решения задания №2.2.
 
     return discountSum;
@@ -49,9 +55,12 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
 
     // присвойте totalSum значение productsSum
     // уменьшите totalSum на discountSum
-
+    let totalSum;
+    totalSum = productsSum;
+    totalSum = totalSum - discountSum;
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
-
+    totalSum = totalSum + shippingSum;
+    let freeShipping = !Boolean(shippingSum);
     // прибавьте к totalSum значение shippingSum
 
     // создайте переменную freeShipping
