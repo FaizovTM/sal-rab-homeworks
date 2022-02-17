db1c163ba6f01cbf9a9a2e6d6dcd71727ebf862f
Timur Faizov <zorki2020@gmail.com>
2022-02-17 21:40:45 +0500

    task final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -36,16 +36,15 @@
 function sendRequest(name, phone, address, goods, sum) {
 
     let countOfGoods = goods.length;
+    let data = {client: "", order: {}, goods: []};
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+    for (let i = 0; i < countOfGoods; i++) {
+        data.goods.push({ title: goods[i].title, count: goods[i].count });
+      }
+    data.client = name + ' ' + phone;
+    data.order.sum = sum;
+    data.order.address = 'ул. ' + address.street + ', дом ' + address.house + ', ' + address.entrance + ' подъезд, ' + address.floor + ' этаж, кв ' + address.flat;
+    let jsonData = JSON.stringify({data});
     
     return jsonData;
 }
